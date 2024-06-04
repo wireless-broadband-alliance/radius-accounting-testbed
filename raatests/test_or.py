0f3a3eb821b1213014116237d8a361777ac0e393
Ryan Blossom <rblossom@singledigits.com>
2024-06-04 12:15:13 -0500

    add openroaming tests

---
+++ raatests/test_or.py
@@ -0,0 +1,17 @@
+import raatestbed.pcap_extract as pe
+from scapy.all import Radius
+from typing import List
+import re
+import pytest
+
+class TestOpenroamAttributes:
+  def test_operator_name_format(self, packets: List[Radius]):
+        """Go through each packet and check if Operator-Name is present"""
+        for packet in packets:
+            operator_names = pe.get_values_for_attribute(packet, 126)
+            # Check only one Operator-Name is present
+            assert len(operator_names) == 1
+            # Check operator-name is in correct format
+            assert re.match(r"4.+:[A-Z]{2}", operator_names[0])
+
+
