39f523547bb682c4eaac8ee22cf7774fa303d4c2
Ryan Blossom <rblossom@singledigits.com>
2024-06-05 14:32:54 -0500

    Disable exception catching
    It is not needed

--- run_tests.py
+++ run_tests.py
@@ -6,10 +6,7 @@ def plain_pytest():
     pytest_args = ["-v", "raatests"]
     extra_args = sys.argv[1:]
     args = pytest_args + extra_args
-    try:
-        pytest.main(args)
-    except Exception as e:
-        print(f"Error: {e}")
+    pytest.main(args)
 
 
 def main():
