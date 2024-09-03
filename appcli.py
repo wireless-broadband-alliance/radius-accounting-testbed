6c1309c8f84a316641ef44c0e93555bab2e02c42
Ryan Blossom <rblossom@singledigits.com>
2024-09-03 10:57:59 -0500

    add upload/download option to cli

--- appcli.py
+++ appcli.py
@@ -5,6 +5,7 @@ import raatestbed.test_setup as ts
 import logging
 import pytest
 import yaml
+import sys
 from raatestbed.metadata import get_metadata
 import raatestbed.defaults as defaults
 from raatestbed.files import init_dirs
@@ -17,10 +18,28 @@ def parse_cliargs():
     parser = argparse.ArgumentParser()
     parser.add_argument("test_name", type=str, help="Name of the test to run")
     parser.add_argument(
-        "data_server_ip", type=str, help="IP of the server to get data from"
+        "--download_data_server_ip",
+        type=str,
+        default=None,
+        help="IP of the server to download data from",
     )
     parser.add_argument(
-        "data_server_port", type=int, help="Port of the server to get data from"
+        "--download_data_server_port",
+        type=int,
+        default=None,
+        help="Port of the server to download data from",
+    )
+    parser.add_argument(
+        "--upload_data_server_ip",
+        type=str,
+        default=None,
+        help="IP of the server to upload data to",
+    )
+    parser.add_argument(
+        "--upload_data_server_port",
+        type=int,
+        default=None,
+        help="Port of the server to upload data to",
     )
     parser.add_argument(
         "--config", type=str, help="Optional config file to get input from"
@@ -96,6 +115,10 @@ def parse_cliargs():
     parser.add_argument(
         "--no_test", action="store_true", help="Skip test case execution"
     )
+    parser.add_argument("--no_upload", action="store_true", help="No not upload chunks")
+    parser.add_argument(
+        "--no_download", action="store_true", help="Do not download chunks"
+    )
     return parser.parse_args()
 
 
@@ -124,7 +147,7 @@ def execute_test_cases(config: ts.TestConfig, logger: logging.Logger):
         pytest.main(pytest_args + extra_args)
 
 
-def get_input_value(cliarg, default_value, configarg=None):
+def get_input_value(cliarg, default_value, configarg):
     """Logic to decide which value to use based on the input."""
     # Priority: CLI > Config > Default
     if cliarg:
@@ -142,114 +165,140 @@ def change_marker_format(markers: str, delim=",") -> list:
     return markers.split(delim)
 
 
-def get_testconfig_with_config_file(cliargs, configargs) -> ts.TestConfig:
+def get_testconfig(cliargs, configargs) -> ts.TestConfig:
     """Create TestConfig object with CLI args WITH config file."""
     cliargs["markers"] = change_marker_format(cliargs["markers"])
     config = ts.TestConfig(
         test_name=cliargs["test_name"],
-        data_server_ip=cliargs["data_server_ip"],
-        data_server_port=cliargs["data_server_port"],
+        upload_data_server_ip=get_input_value(
+            cliargs["upload_data_server_ip"],
+            defaults.UPLOAD_DATA_SERVER_IP,
+            configargs.get("upload_data_server_ip"),
+        ),
+        upload_data_server_port=get_input_value(
+            cliargs["upload_data_server_port"],
+            defaults.UPLOAD_DATA_SERVER_PORT,
+            configargs.get("upload_data_server_port"),
+        ),
+        download_data_server_ip=get_input_value(
+            cliargs["download_data_server_ip"],
+            defaults.DOWNLOAD_DATA_SERVER_IP,
+            configargs.get("download_data_server_ip"),
+        ),
+        download_data_server_port=get_input_value(
+            cliargs["download_data_server_port"],
+            defaults.DOWNLOAD_DATA_SERVER_PORT,
+            configargs.get("download_data_server"),
+        ),
         chunk_size=int(
             get_input_value(
-                cliargs["chunk_size"], defaults.CHUNK_SIZE, configargs["chunk_size"]
+                cliargs["chunk_size"], defaults.CHUNK_SIZE, configargs.get("chunk_size")
             )
         ),
         chunks=int(
             get_input_value(
-                cliargs["chunks"], defaults.CHUNK_SIZE, configargs["chunks"]
+                cliargs["chunks"], defaults.CHUNK_SIZE, configargs.get("chunks")
             )
         ),
         sut_brand=get_input_value(
-            cliargs["sut_brand"], defaults.SUT, configargs["sut_brand"]
+            cliargs["sut_brand"], defaults.SUT, configargs.get("sut_brand")
         ),
         sut_hardware=get_input_value(
-            cliargs["sut_hardware"], defaults.SUT, configargs["sut_hardware"]
+            cliargs["sut_hardware"], defaults.SUT, configargs.get("sut_hardware")
         ),
         sut_software=get_input_value(
-            cliargs["sut_software"], defaults.SUT, configargs["sut_software"]
+            cliargs["sut_software"], defaults.SUT, configargs.get("sut_software")
         ),
         data_server_listen_port=int(
             get_input_value(
                 cliargs["data_server_listen_port"],
                 defaults.DATA_SERVER_LISTEN_PORT,
-                configargs["data_server_listen_port"],
+                configargs.get("data_server_listen_port"),
             )
         ),
-        ssid=get_input_value(cliargs["ssid"], defaults.SSID, configargs["ssid"]),
+        ssid=get_input_value(cliargs["ssid"], defaults.SSID, configargs.get("ssid")),
         generate_pcap=get_input_value(
             not cliargs["no_pcap"],
             defaults.GENERATE_PCAP,
-            configargs["generate_pcap"],
+            configargs.get("generate_pcap"),
         ),
         generate_report=get_input_value(
             not cliargs["no_test"],
             defaults.GENERATE_REPORT,
-            configargs["generate_report"],
+            configargs.get("generate_report"),
         ),
         upload_chunks=get_input_value(
             not cliargs["no_upload"],
             defaults.UPLOAD_CHUNKS,
-            configargs["upload_chunks"],
+            configargs.get("upload_chunks"),
         ),
         download_chunks=get_input_value(
             not cliargs["no_download"],
             defaults.DOWNLOAD_CHUNKS,
-            configargs["download_chunks"],
+            configargs.get("download_chunks"),
         ),
-        markers=get_input_value(cliargs["markers"], configargs["markers"], TEST_TAGS),
+        markers=get_input_value(
+            cliargs["markers"], configargs.get("markers"), TEST_TAGS
+        ),
         client_interface=get_input_value(
             cliargs["client_interface"],
             defaults.WIRELESS_IFACE,
-            configargs["client_interface"],
+            configargs.get("client_interface"),
         ),
         server_interface=get_input_value(
             cliargs["server_interface"],
             defaults.WIRED_IFACE,
-            configargs["server_interface"],
+            configargs.get("server_interface"),
         ),
         local_output_directory=get_input_value(
             cliargs["local_output_directory"],
             defaults.ROOT_DIR,
-            configargs["local_output_directory"],
+            configargs.get("local_output_directory"),
         ),
     )
     return config
 
 
-def get_testconfig_without_config_file(cliargs) -> ts.TestConfig:
-    """Create TestConfig object with CLI args WITHOUT config file."""
-    cliargs["markers"] = change_marker_format(cliargs["markers"])
-    config = ts.TestConfig(
-        test_name=cliargs["test_name"],
-        data_server_ip=cliargs["data_server_ip"],
-        data_server_port=cliargs["data_server_port"],
-        chunk_size=int(get_input_value(cliargs["chunk_size"], defaults.CHUNK_SIZE)),
-        chunks=int(get_input_value(cliargs["chunks"], defaults.CHUNKS)),
-        data_server_listen_port=int(
-            get_input_value(
-                cliargs["data_server_listen_port"], defaults.DATA_SERVER_LISTEN_PORT
-            )
-        ),
-        ssid=get_input_value(cliargs["ssid"], defaults.SSID),
-        generate_pcap=get_input_value(not cliargs["no_pcap"], defaults.GENERATE_PCAP),
-        generate_report=get_input_value(
-            not cliargs["no_test"], defaults.GENERATE_REPORT
-        ),
-        markers=get_input_value(cliargs["markers"], TEST_TAGS),
-        client_interface=get_input_value(
-            cliargs["client_interface"], defaults.WIRELESS_IFACE
-        ),
-        server_interface=get_input_value(
-            cliargs["server_interface"], defaults.WIRED_IFACE
-        ),
-        local_output_directory=get_input_value(
-            cliargs["local_output_directory"], defaults.ROOT_DIR
-        ),
-        sut_brand=get_input_value(cliargs["sut_brand"], defaults.SUT),
-        sut_hardware=get_input_value(cliargs["sut_hardware"], defaults.SUT),
-        sut_software=get_input_value(cliargs["sut_software"], defaults.SUT),
-    )
-    return config
+# def get_testconfig_without_config_file(cliargs) -> ts.TestConfig:
+#    """Create TestConfig object with CLI args WITHOUT config file."""
+#    cliargs["markers"] = change_marker_format(cliargs["markers"])
+#    config = ts.TestConfig(
+#        test_name=cliargs["test_name"],
+#        upload_data_server_ip=cliargs["upload_data_server_ip"],
+#        upload_data_server_port=cliargs["upload_data_server_port"],
+#        download_data_server_ip=cliargs["download_data_server_ip"],
+#        download_data_server_port=cliargs["download_data_server_port"],
+#        chunk_size=int(get_input_value(cliargs["chunk_size"], defaults.CHUNK_SIZE)),
+#        chunks=int(get_input_value(cliargs["chunks"], defaults.CHUNKS)),
+#        data_server_listen_port=int(
+#            get_input_value(
+#                cliargs["data_server_listen_port"], defaults.DATA_SERVER_LISTEN_PORT
+#            )
+#        ),
+#        ssid=get_input_value(cliargs["ssid"], defaults.SSID),
+#        generate_pcap=get_input_value(not cliargs["no_pcap"], defaults.GENERATE_PCAP),
+#        generate_report=get_input_value(
+#            not cliargs["no_test"], defaults.GENERATE_REPORT
+#        ),
+#        upload_chunks=get_input_value(not cliargs["no_upload"], defaults.UPLOAD_CHUNKS),
+#        download_chunks=get_input_value(
+#            not cliargs["no_download"], defaults.DOWNLOAD_CHUNKS
+#        ),
+#        markers=get_input_value(cliargs["markers"], TEST_TAGS),
+#        client_interface=get_input_value(
+#            cliargs["client_interface"], defaults.WIRELESS_IFACE
+#        ),
+#        server_interface=get_input_value(
+#            cliargs["server_interface"], defaults.WIRED_IFACE
+#        ),
+#        local_output_directory=get_input_value(
+#            cliargs["local_output_directory"], defaults.ROOT_DIR
+#        ),
+#        sut_brand=get_input_value(cliargs["sut_brand"], defaults.SUT),
+#        sut_hardware=get_input_value(cliargs["sut_hardware"], defaults.SUT),
+#        sut_software=get_input_value(cliargs["sut_software"], defaults.SUT),
+#    )
+#    return config
 
 
 def main():
@@ -259,28 +308,30 @@ def main():
     logger = logging.getLogger(__name__)
     ts.setup_logging(cliargs_orig.debug)
 
-    # Create TestConfig object using CLI args WITH config file
-    if cliargs["config"]:
-        # config_file = ts.read_config_file(cliargs["config"])
+    # Check that either upload or download data server IP is provided.
+    if not cliargs["upload_data_server_ip"] and not cliargs["download_data_server_ip"]:
+        logger.error("Either upload or download data server IP must be provided.")
+        sys.exit(1)
+
+    # Create TestConfig object
+    if not cliargs["config"]:
+        configargs = {}
+    else:
         config_file = cliargs["config"]
         with open(config_file, "r") as file:
             configargs = yaml.safe_load(file)
-        config = get_testconfig_with_config_file(cliargs, configargs)
-
-    # Create TestConfig object using CLI args WITHOUT config file
-    else:
-        config = get_testconfig_without_config_file(cliargs)
+    config = get_testconfig(cliargs, configargs)
 
     # Create directories
     logger.info(f"Creating subdirectories in {config.local_output_directory}")
     init_dirs(config.local_output_directory)
     config.write_yaml()
 
-    # Generate PCAP.
+    # Generate PCAP if enabled.
     if config.generate_pcap:
         ts.generate_pcap(config, logger)
 
-    # Execute tests.
+    # Execute tests if enabled.
     if config.generate_report:
         execute_test_cases(config, logger)
 
