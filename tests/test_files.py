f3b63014637048c26c644c6367c62e895c814480
Ryan Blossom <rblossom@singledigits.com>
2024-12-30 08:10:08 -0600

    Add unit tests for files.py

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,10 +1,39 @@
 
+from src.files import init_dirs, SUBDIRS, get_marker_list
 import src.files as files
 import os
 
 def test_init_dirs():
-    files.init_dirs(root_dir="/tmp")
-    #check that pcap subdirectory exists in /tmp
-    
+    """Test subdirectories are created."""
+    root_dir = "/tmp/raatest"
+    init_dirs(root_dir=root_dir)
+    for _, subdir in SUBDIRS.items():
+        assert os.path.exists(os.path.join(root_dir, subdir))
 
-#def test_get_marker_list
+def test_get_marker_list():
+    """Test correct markers are read from pytest.ini file."""
+    expected_markers = ["marker_a", "marker_b", "marker_c", "marker_d"]
+    markers = get_marker_list(ini_file="tests/data/configs/pytest.ini")
+    assert len(markers) == len(expected_markers)
+    for expected_marker in expected_markers:
+        assert expected_marker in markers
+
+def test_file_creation():
+    """Test files can be created in subdirectories."""
+    root_dir = "/tmp/raatest2"
+    test_name = "test"
+    init_dirs(root_dir=root_dir)
+    files_to_create = [
+        files.get_metadata_filename(test_name=test_name, root_dir=root_dir),
+        files.get_pcap_filename(test_name=test_name, root_dir=root_dir),
+        files.get_report_filename(test_name=test_name, root_dir=root_dir),
+        files.get_config_filename(test_name=test_name, root_dir=root_dir),
+        files.get_freeradius_log_filename(test_name=test_name, root_dir=root_dir),
+        files.get_wpasupplicant_log_filename(test_name=test_name, root_dir=root_dir),
+        files.get_tcpdump_log_filename(test_name=test_name, root_dir=root_dir),
+        files.get_zipped_bundle_filename(test_name=test_name, root_dir=root_dir),
+    ]
+    # Create files
+    for file in files_to_create:
+        with open(file, "w") as f:
+            f.write("test")
