6c1efd235a8c688d8dbace75191853b055b0cc7f
Ryan Blossom <rblossom@singledigits.com>
2025-01-10 15:57:59 -0600

    Add easier way to create args in defaults.py

--- src/defaults.py
+++ src/defaults.py
@@ -1,6 +1,23 @@
-"""Contains default values."""
+"""Contains default values. This file contains single source of truth for possible input values."""
 
 import os
+from collections import ChainMap
+
+KEY_ROOT_DIR = "ROOT_DIR"
+KEY_WIRELESS_IFACE = "WIRELESS_IFACE"
+KEY_WIRED_IFACE = "WIRED_IFACE"
+KEY_CHUNK_SIZE = "CHUNK_SIZE"
+KEY_SSID = "SSID"
+KEY_DATA_SERVER_LISTEN_PORT = "DATA_SERVER_LISTEN_PORT"
+KEY_DATA_SERVER_PORT = "DATA_SERVER_PORT"
+KEY_CHUNKS = "CHUNKS"
+KEY_SUT = "SUT"
+KEY_GENERATE_PCAP = "GENERATE_PCAP"
+KEY_GENERATE_REPORT = "GENERATE_REPORT"
+KEY_UPLOAD_CHUNKS = "UPLOAD_CHUNKS"
+KEY_DOWNLOAD_CHUNKS = "DOWNLOAD_CHUNKS"
+KEY_DATA_SERVER_IP = "DATA_SERVER_IP"
+KEY_RELATIVE_PYTEST_INI = "RELATIVE_PYTEST_INI"
 
 ROOT_DIR = "/usr/local/raa"
 WIRELESS_IFACE = "wlan0"
@@ -17,3 +34,37 @@ UPLOAD_CHUNKS = True
 DOWNLOAD_CHUNKS = True
 DATA_SERVER_IP = "10.10.10.10"
 RELATIVE_PYTEST_INI = os.path.join("raatests", "pytest.ini")
+
+def get_defaults() -> dict:
+   """Return the default values. These take lowest priority."""
+   defaults = {
+     KEY_ROOT_DIR: ROOT_DIR,
+     KEY_WIRELESS_IFACE: WIRELESS_IFACE,
+     KEY_WIRED_IFACE: WIRED_IFACE,
+     KEY_CHUNK_SIZE: CHUNK_SIZE,
+     KEY_SSID: SSID,
+     KEY_DATA_SERVER_LISTEN_PORT: DATA_SERVER_LISTEN_PORT,
+     KEY_DATA_SERVER_PORT: DATA_SERVER_PORT,
+     KEY_CHUNKS: CHUNKS,
+     KEY_SUT: SUT,
+     KEY_GENERATE_PCAP: GENERATE_PCAP,
+     KEY_GENERATE_REPORT: GENERATE_REPORT,
+     KEY_UPLOAD_CHUNKS: UPLOAD_CHUNKS,
+     KEY_DOWNLOAD_CHUNKS: DOWNLOAD_CHUNKS,
+     KEY_DATA_SERVER_IP: DATA_SERVER_IP,
+     KEY_RELATIVE_PYTEST_INI: RELATIVE_PYTEST_INI,
+   }
+   return defaults
+
+def get_all_args(*dicts) -> ChainMap:
+    """Merge dictionaries from different arguments (e.g., CLI, config, default)."""
+    #Raise exception if unknown key is found
+    new_dicts = []
+    defaults = get_defaults()
+    for _dict in dicts:
+        for key in _dict.keys():
+            if key not in defaults.keys():
+                raise KeyError(f"Unknown key {key} found in dictionary {_dict}")
+        new_dicts.append(_dict)
+    new_dicts.append(defaults)
+    return ChainMap(*new_dicts)
---
+++ tests/test_defaults.py
@@ -0,0 +1,54 @@
+import pytest
+import src.defaults as defaults
+
+@pytest.fixture()
+def cliargs():
+    cliargs = {
+      defaults.KEY_WIRED_IFACE: "eth_fron_cli",
+      defaults.KEY_WIRELESS_IFACE: "wlan_from_cli",
+      defaults.KEY_SSID: "ssid_from_cli",
+      defaults.KEY_GENERATE_PCAP: True,
+      defaults.KEY_DOWNLOAD_CHUNKS: True,
+    }
+    return cliargs
+
+@pytest.fixture()
+def configargs():
+    configargs = {
+      defaults.KEY_WIRED_IFACE: "eth_fron_config",
+      defaults.KEY_WIRELESS_IFACE: "wlan_from_config",
+      defaults.KEY_SSID: "ssid_from_config",
+      defaults.KEY_GENERATE_PCAP: False,
+      defaults.KEY_DOWNLOAD_CHUNKS: False,
+      defaults.KEY_RELATIVE_PYTEST_INI: "ini_from_config",
+      defaults.KEY_DATA_SERVER_IP: "ip_from_config",
+    }
+    return configargs
+
+def test_arg_presense(cliargs, configargs):
+  default_args = defaults.get_defaults()
+  final = defaults.get_all_args(cliargs, configargs)
+  #Check if all keys are present
+  for key in default_args.keys():
+    assert key in final.keys()
+
+def test_arg_priority(cliargs, configargs):
+  """Test combination of args."""
+  #Priority: CLI > Config > Default
+  default_args = defaults.get_defaults()
+  final = defaults.get_all_args(cliargs, configargs)
+
+  #CLI args should take priority over both config and default args
+  assert final[defaults.KEY_WIRED_IFACE] == cliargs[defaults.KEY_WIRED_IFACE]
+  assert final[defaults.KEY_WIRELESS_IFACE] == cliargs[defaults.KEY_WIRELESS_IFACE]
+  assert final[defaults.KEY_SSID] == cliargs[defaults.KEY_SSID]
+  assert final[defaults.KEY_GENERATE_PCAP] == cliargs[defaults.KEY_GENERATE_PCAP]
+  assert final[defaults.KEY_DOWNLOAD_CHUNKS] == cliargs[defaults.KEY_DOWNLOAD_CHUNKS]
+
+  #Config args should take priority over default
+  assert final[defaults.KEY_DATA_SERVER_IP] == configargs[defaults.KEY_DATA_SERVER_IP]
+  assert final[defaults.KEY_RELATIVE_PYTEST_INI] == configargs[defaults.KEY_RELATIVE_PYTEST_INI]
+
+  #Default args should be used if not provided in CLI or config
+  assert final[defaults.KEY_CHUNK_SIZE] == default_args[defaults.KEY_CHUNK_SIZE]
+  assert final[defaults.KEY_CHUNKS] == default_args[defaults.KEY_CHUNKS]
